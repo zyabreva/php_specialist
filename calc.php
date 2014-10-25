1662c621e5335234918ebe986d15028c02b94173
zyabreva <alice1024@yandex.ru>
2014-10-25 14:56:57 +0400

    comment text here

---
+++ calc.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
+	<head>
+		<title>Калькулятор</title>
+		<meta http-equiv="content-type"
+			content="text/html; charset=windows-1251" />
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+	<body>
+
+		<div id="header">
+			<!-- Верхняя часть страницы -->
+			<img src="logo.gif" width="187" height="29" alt="Наш логотип" class="logo" />
+			<span class="slogan">приходите к нам учиться</span>
+			<!-- Верхняя часть страницы -->
+		</div>
+
+		<div id="content">
+			<!-- Заголовок -->
+			<h1>Калькулятор школьника</h1>
+			<!-- Заголовок -->
+			<!-- Область основного контента -->
+			<form action=''>
+				<label>Число 1:</label><br />
+				<input name='num1' type='text'/><br />
+				<label>Оператор: </label><br />
+				<input name='operator' type='text'/><br />
+				<label>Число 2: </label><br />
+				<input name='num2' type='text'/><br /><br />
+				<input type='submit' value='Считать'>
+			</form>	
+			<!-- Область основного контента -->
+		</div>
+		<div id="nav">
+			<h2>Навигация по сайту</h2>
+			<!-- Меню -->
+			<ul>
+				<li><a href='index.php'>Домой</a></li>
+				<li><a href='about.php'>О нас</a></li>
+				<li><a href='contact.php'>Контакты</a></li>
+				<li><a href='table.php'>Таблица умножения</a></li>
+				<li><a href='calc.php'>Калькулятор</a></li>
+			</ul>
+			<!-- Меню -->
+		</div>
+		<div id="footer">
+			<!-- Нижняя часть страницы -->
+			&copy; Супер Мега Веб-мастер, 2000 - 2012
+			<!-- Нижняя часть страницы -->
+		</div>
+	</body>
+</html>
---
+++ contact.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
+	<head>
+		<title>Контакты</title>
+		<meta http-equiv="content-type"
+			content="text/html; charset=windows-1251" />
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+	<body>
+
+		<div id="header">
+			<!-- Верхняя часть страницы -->
+			<img src="logo.gif" width="187" height="29" alt="Наш логотип" class="logo" />
+			<span class="slogan">приходите к нам учиться</span>
+			<!-- Верхняя часть страницы -->
+		</div>
+
+		<div id="content">
+			<!-- Заголовок -->
+			<h1>Обратная связь</h1>
+			<!-- Заголовок -->
+			<!-- Область основного контента -->
+			<h3>Адрес</h3>
+			<p>123456 Москва, Малый Американский переулок 21</p>
+			<h3>Задайте вопрос</h3>
+			<form action='' method='post'>
+				<label>Тема письма: </label><br />
+				<input name='subject' type='text' size="50"/><br />
+				<label>Содержание: </label><br />
+				<textarea name='body' cols="50" rows="10"></textarea><br /><br />
+				<input type='submit' value='Отправить' />
+			</form>	
+			<!-- Область основного контента -->
+		</div>
+		<div id="nav">
+			<h2>Навигация по сайту</h2>
+			<!-- Меню -->
+			<ul>
+				<li><a href='index.php'>Домой</a></li>
+				<li><a href='about.php'>О нас</a></li>
+				<li><a href='contact.php'>Контакты</a></li>
+				<li><a href='table.php'>Таблица умножения</a></li>
+				<li><a href='calc.php'>Калькулятор</a></li>
+			</ul>
+			<!-- Меню -->
+		</div>
+		<div id="footer">
+			<!-- Нижняя часть страницы -->
+			&copy; Супер Мега Веб-мастер, 2000 - 2012
+			<!-- Нижняя часть страницы -->
+		</div>
+	</body>
+</html>
