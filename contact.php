3ed269d11eadd42424d057b91c093af908b4f320
zyabreva <alice1024@yandex.ru>
2014-10-27 08:53:58 +0300

    lab 2.6 switch

--- contact.php
+++ contact.php
@@ -1,3 +1,14 @@
+<?php 
+$size = ini_get('post_max_size');
+$letter = $size{strlen($size)-1};
+$size = (int)$size;
+switch (strtoupper($letter)){
+case 'G': $size *=1024; 
+case 'M': $size *=1024; 
+case 'K': $size *=1024;
+}
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
 	<head>
@@ -28,8 +39,10 @@
 				<input name='subject' type='text' size="50"/><br />
 				<label>Содержание: </label><br />
 				<textarea name='body' cols="50" rows="10"></textarea><br /><br />
+				
 				<input type='submit' value='Отправить' />
 			</form>	
+			<p>Максимальный размер: <?=$size;?> байт</p>
 			<!-- Область основного контента -->
 		</div>
 		<div id="nav">
